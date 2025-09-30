b4c3f2445cdccafb64c95a16ddd79a5a83c7f843
JoelAlgera <Joel.Algera@student.ufv.ca>
2025-09-30 15:13:09 -0700

    Generalization of Functions
    Makes it easy for multiple biomes to be made, just requires a noise and texture function for each

--- surface.gd
+++ surface.gd
@@ -2,9 +2,9 @@ extends Node3D
 
 #variables determining how peaks are generated
 @export var size:int = 1024
-@export var spacing:float = 1
-#@export var num_peaks:int = 64
+@export var spacing:float = .25
 @export var amplitude:float = 5
+<<<<<<< Updated upstream
 @export var biome = ["Forest", "Alpine","Dunes"]
 
 
@@ -19,10 +19,16 @@ func _ready():
 	var dune_texture = ImageTexture.create_from_image(text_img)
 	#randomize()
 	#_generate_peaks()
+=======
+@export var biome = "Dunes" #Forest, Alpine, Coast, Dunes
+
+func _ready():
+>>>>>>> Stashed changes
 	var mesh_instance = MeshInstance3D.new() #create mesh
 	var mat = StandardMaterial3D.new() #create material for mesh
 	mesh_instance.material_override = mat
 	
+<<<<<<< Updated upstream
 	mesh_instance.material_override.albedo_texture = dune_texture
 	
 	
@@ -32,6 +38,43 @@ func _ready():
 	mat.albedo_texture = dune_texture
 	mesh_instance.mesh = generate_grid_slow(size,.25, img_noise) #generate quad mesh with FNl image
 	mesh_instance.material_override = mat #set Mesh's material
+=======
+	#Biome Specific
+	if biome == "Dunes":
+		amplitude = 3
+		var img_noise = generate_dune_Noise(size)
+		var text_img = create_dune_texture(img_noise)
+		var texture = ImageTexture.create_from_image(text_img)
+		mat.albedo_texture = texture
+		mesh_instance.mesh = generate_grid_slow(size,spacing, img_noise)
+	elif biome == "Forest":
+		amplitude = 3 #desired amplitude for your biome
+		var img_noise = generate_dune_Noise(size) #Your biomes noise func
+		var text_img = create_dune_texture(img_noise) #Your biomes texture func
+		var texture = ImageTexture.create_from_image(text_img)
+		mat.albedo_texture = texture
+		mesh_instance.mesh = generate_grid_slow(size,spacing, img_noise)
+	elif biome == "Alpine":
+		amplitude = 3 #desired amplitude for your biome
+		var img_noise = generate_dune_Noise(size) #Your biomes noise func
+		var text_img = create_dune_texture(img_noise) #Your biomes texture func
+		var texture = ImageTexture.create_from_image(text_img)
+		mat.albedo_texture = texture
+		mesh_instance.mesh = generate_grid_slow(size,spacing, img_noise)
+	elif biome == "Coast":
+		amplitude = 3 #desired amplitude for your biome
+		var img_noise = generate_dune_Noise(size) #Your biomes noise func
+		var text_img = create_dune_texture(img_noise) #Your biomes texture func
+		var texture = ImageTexture.create_from_image(text_img)
+		mat.albedo_texture = texture
+		mesh_instance.mesh = generate_grid_slow(size,spacing, img_noise)
+	else: 
+		var img_noise = generate_FNL_Noise(size)
+		var text_img = create_texture(img_noise)
+		var texture = ImageTexture.create_from_image(text_img)
+		mat.albedo_texture = texture
+		mesh_instance.mesh = generate_grid_slow(size,spacing, img_noise)
+>>>>>>> Stashed changes
 	add_child(mesh_instance) #add mesh to world
 	
 	
@@ -94,30 +137,18 @@ func generate_grid_slow(size, spacing, FNL): #creates size x size grid of points
 	st.generate_normals() # normals point perpendicular up from each face
 	print(vertex_count/4, " quads printed")
 	return st.commit() # arranges mesh data structures into arrays 
-	
-	
-	#function for generating peaks randomly
-#func _generate_peaks():
-	#peaks.clear()
-	#for i in range(num_peaks):
-		#var px = randi() % size
-		#var py = randi() % size
-		#var ph = randf_range(-amplitude, amplitude)
-		#peaks.append({ "pos": Vector2(px, py), "h": ph })
-#
-##sets point's heights around peaks to gradually approach them
-#func get_height(x:int, y:int) -> float:
-	#var total_weight = 0.0
-	#var weighted_height = 0.0
-	#for peak in peaks:
-		#var dist = peak["pos"].distance_to(Vector2(x,y)) + 0.001
-		#var w = 1.0 / dist
-		#total_weight += w
-		#weighted_height += peak["h"] * w
-	#return weighted_height / total_weight
+func get_FNL_Height(x,y, FNL): #takes each pixel, finds the grayscale value [-1,1] and multiplies by amplitude to increase/decrease elevation 
+	var tone = FNL.get_pixel(x,y)
+	var toneVal = tone.r
+	return (toneVal * 3.5 - 1.0) * amplitude
 
+<<<<<<< Updated upstream
 func generate_dune_Noise(size): #Generates the dune noise map, this can be heavily tweaked to get ideal look for water or mountians etc
 	
+=======
+
+func generate_dune_Noise(size): #Generates the dune noise map, this can be heavily tweaked to get ideal look for water or mountians etc
+>>>>>>> Stashed changes
 	var dune_noise = FastNoiseLite.new()
 	dune_noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	dune_noise.fractal_type = FastNoiseLite.FRACTAL_FBM
@@ -127,6 +158,7 @@ func generate_dune_Noise(size): #Generates the dune noise map, this can be heavi
 
 	var img =  dune_noise.get_seamless_image(size,size)
 	save_png(img, "res://debug_noise.png")
+<<<<<<< Updated upstream
 	return img
 
 func get_FNL_Height(x,y, FNL): #takes each pixel, finds the grayscale value [-1,1] and multiplies by amplitude to increase/decrease elevation 
@@ -135,6 +167,25 @@ func get_FNL_Height(x,y, FNL): #takes each pixel, finds the grayscale value [-1,
 	var dune_tone = FNL.get_pixel(x,y)
 	var dune_toneVal = dune_tone.r
 	return (dune_toneVal * 3.5 - 1.0) * amplitude
+
+func create_dune_texture(FNL):
+	var dune_texture = Image.create_empty(size,size, false, Image.FORMAT_RGBA8)
+=======
+	return img
+func generate_forest_Noise(size):
+	pass
+func generate_alpine_Noise(size):
+	pass
+func generate_FNL_Noise(size):
+	var noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.fractal_type = FastNoiseLite.FRACTAL_FBM
+	noise.fractal_octaves = 5
+	noise.fractal_gain = 0.5
+	var img =  noise.get_seamless_image(size,size)
+	save_png(img, "res://debug_noise.png")
+	return img
+
 
 func create_dune_texture(FNL):
 	var dune_texture = Image.create_empty(size,size, false, Image.FORMAT_RGBA8)
@@ -151,7 +202,35 @@ func create_dune_texture(FNL):
 				dune_texture.set_pixel(x,y, Color(0.18, 0.302, 0.529, 1.0)) # oasis (lowest)
 	save_png(dune_texture, "res://textMap_DUNE.png")
 	return dune_texture
+func create_forest_texture(FNL):
+	pass
+func create_alpine_texture(FNL):
+	pass
+func create_texture(FNL):
+	var texture = Image.create_empty(size,size, false, Image.FORMAT_RGBA8)
+>>>>>>> Stashed changes
+	for x in range(size):
+		for y in range(size):
+			var dune_tone = FNL.get_pixel(x,y)
+			if (.75 <= dune_tone.r) and (dune_tone.r <= 1):
+				dune_texture.set_pixel(x,y, Color(0.988, 0.741, 0.463, 1.0)) # dune peak (highest)
+			elif (.25 <= dune_tone.r) and (dune_tone.r < .75):
+				dune_texture.set_pixel(x,y, Color(0.886, 0.62, 0.365, 1.0)) # dune middle
+			elif (.15 <= dune_tone.r) and (dune_tone.r < .25):
+				dune_texture.set_pixel(x,y, Color(0.773, 0.522, 0.31, 1.0)) # dune lower
+			else:
+<<<<<<< Updated upstream
+				dune_texture.set_pixel(x,y, Color(0.18, 0.302, 0.529, 1.0)) # oasis (lowest)
+	save_png(dune_texture, "res://textMap_DUNE.png")
+	return dune_texture
 	
+=======
+				texture.set_pixel(x,y, Color(0.0, 0.961, 0.0, 1.0))
+				texture.set_pixel(x,y, Color(0.0, 0.188, 1.0, 1.0))
+	save_png(texture, "res://textMap_BASIC.png")
+	return texture
+
+>>>>>>> Stashed changes
 func save_png(img, path): #saves FNL png for comparison after
 	# Save PNG for debugging (res:// saves into your project folder)
 	var save_path = path
@@ -160,24 +239,3 @@ func save_png(img, path): #saves FNL png for comparison after
 		print("Noise saved to ", save_path)
 	else:
 		print("Error saving noise image: ", err)
-	
-	
-#function for generating peaks randomly
-#func _generate_peaks():
-	#peaks.clear()
-	#for i in range(num_peaks):
-		#var px = randi() % size
-		#var py = randi() % size
-		#var ph = randf_range(-amplitude, amplitude)
-		#peaks.append({ "pos": Vector2(px, py), "h": ph })
-#
-#sets point's heights around peaks to gradually approach them
-#func get_height(x:int, y:int) -> float:
-	#var total_weight = 0.0
-	#var weighted_height = 0.0
-	#for peak in peaks:
-		#var dist = peak["pos"].distance_to(Vector2(x,y)) + 0.001
-		#var w = 1.0 / dist
-		#total_weight += w
-		#weighted_height += peak["h"] * w
-	#return weighted_height / total_weight
