c8985eac7e20d205ee521a89406d8f3761f6f45f
JoelAlgera <Joel.Algera@student.ufv.ca>
2025-09-30 15:23:51 -0700

    Generalization of Functions
    Makes it easy for multiple biomes to be made, just requires a noise and texture function for each

--- surface.gd
+++ surface.gd
@@ -4,42 +4,13 @@ extends Node3D
 @export var size:int = 1024
 @export var spacing:float = .25
 @export var amplitude:float = 5
-<<<<<<< Updated upstream
-@export var biome = ["Forest", "Alpine","Dunes"]
-
-
-
-func _ready():
-	
-	
-	#var img_noise = generate_FNL_Noise(size) #generate noise image
-	var img_noise = generate_dune_Noise(size)
-	var text_img = create_dune_texture(img_noise)
-	#var text_img = create_texture(img_noise)
-	var dune_texture = ImageTexture.create_from_image(text_img)
-	#randomize()
-	#_generate_peaks()
-=======
 @export var biome = "Dunes" #Forest, Alpine, Coast, Dunes
 
 func _ready():
->>>>>>> Stashed changes
 	var mesh_instance = MeshInstance3D.new() #create mesh
 	var mat = StandardMaterial3D.new() #create material for mesh
 	mesh_instance.material_override = mat
 	
-<<<<<<< Updated upstream
-	mesh_instance.material_override.albedo_texture = dune_texture
-	
-	
-	#mat.cull_mode = BaseMaterial3D.CULL_DISABLED 
-	#mat.albedo_color = Color(0.314, 0.784, 0.216, 1.0) #set color to green
-	
-	mat.albedo_texture = dune_texture
-	mesh_instance.mesh = generate_grid_slow(size,.25, img_noise) #generate quad mesh with FNl image
-	mesh_instance.material_override = mat #set Mesh's material
-=======
-	#Biome Specific
 	if biome == "Dunes":
 		amplitude = 3
 		var img_noise = generate_dune_Noise(size)
@@ -74,10 +45,7 @@ func _ready():
 		var texture = ImageTexture.create_from_image(text_img)
 		mat.albedo_texture = texture
 		mesh_instance.mesh = generate_grid_slow(size,spacing, img_noise)
->>>>>>> Stashed changes
 	add_child(mesh_instance) #add mesh to world
-	
-	
 	pass
 
 
@@ -142,13 +110,9 @@ func get_FNL_Height(x,y, FNL): #takes each pixel, finds the grayscale value [-1,
 	var toneVal = tone.r
 	return (toneVal * 3.5 - 1.0) * amplitude
 
-<<<<<<< Updated upstream
-func generate_dune_Noise(size): #Generates the dune noise map, this can be heavily tweaked to get ideal look for water or mountians etc
-	
-=======
+
 
 func generate_dune_Noise(size): #Generates the dune noise map, this can be heavily tweaked to get ideal look for water or mountians etc
->>>>>>> Stashed changes
 	var dune_noise = FastNoiseLite.new()
 	dune_noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	dune_noise.fractal_type = FastNoiseLite.FRACTAL_FBM
@@ -158,20 +122,9 @@ func generate_dune_Noise(size): #Generates the dune noise map, this can be heavi
 
 	var img =  dune_noise.get_seamless_image(size,size)
 	save_png(img, "res://debug_noise.png")
-<<<<<<< Updated upstream
+
 	return img
 
-func get_FNL_Height(x,y, FNL): #takes each pixel, finds the grayscale value [-1,1] and multiplies by amplitude to increase/decrease elevation 
-	if biome[2] == "Dunes":
-		amplitude = 3.5
-	var dune_tone = FNL.get_pixel(x,y)
-	var dune_toneVal = dune_tone.r
-	return (dune_toneVal * 3.5 - 1.0) * amplitude
-
-func create_dune_texture(FNL):
-	var dune_texture = Image.create_empty(size,size, false, Image.FORMAT_RGBA8)
-=======
-	return img
 func generate_forest_Noise(size):
 	pass
 func generate_alpine_Noise(size):
@@ -207,30 +160,8 @@ func create_forest_texture(FNL):
 func create_alpine_texture(FNL):
 	pass
 func create_texture(FNL):
-	var texture = Image.create_empty(size,size, false, Image.FORMAT_RGBA8)
->>>>>>> Stashed changes
-	for x in range(size):
-		for y in range(size):
-			var dune_tone = FNL.get_pixel(x,y)
-			if (.75 <= dune_tone.r) and (dune_tone.r <= 1):
-				dune_texture.set_pixel(x,y, Color(0.988, 0.741, 0.463, 1.0)) # dune peak (highest)
-			elif (.25 <= dune_tone.r) and (dune_tone.r < .75):
-				dune_texture.set_pixel(x,y, Color(0.886, 0.62, 0.365, 1.0)) # dune middle
-			elif (.15 <= dune_tone.r) and (dune_tone.r < .25):
-				dune_texture.set_pixel(x,y, Color(0.773, 0.522, 0.31, 1.0)) # dune lower
-			else:
-<<<<<<< Updated upstream
-				dune_texture.set_pixel(x,y, Color(0.18, 0.302, 0.529, 1.0)) # oasis (lowest)
-	save_png(dune_texture, "res://textMap_DUNE.png")
-	return dune_texture
-	
-=======
-				texture.set_pixel(x,y, Color(0.0, 0.961, 0.0, 1.0))
-				texture.set_pixel(x,y, Color(0.0, 0.188, 1.0, 1.0))
-	save_png(texture, "res://textMap_BASIC.png")
-	return texture
+	pass
 
->>>>>>> Stashed changes
 func save_png(img, path): #saves FNL png for comparison after
 	# Save PNG for debugging (res:// saves into your project folder)
 	var save_path = path
